75f75348af8128879941863ed11ce50eb01019eb
Juliet <agutu@gmail.com>
2022-04-08 00:19:21 +0300

    create register under views.py file

--- awwardsproject/views.py
+++ awwardsproject/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def index(request):
+
+    return render(request, 'awwardsproject/index.html')
+
+
+
+# User registration 
+
+def register(request):
+    form = UserCreationForm
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User has been registered')
+
+            return redirect ('/accounts/login')
+        else:
+            return render(request, 'registration/registration_form.html', {'form': form})
+
+    else:
+        return render(request, 'registration/registration_form.html', {'form': form})    
+
+
