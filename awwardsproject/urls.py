635c8e5f1f135744baab53845381d55963bb0cb4
Juliet <agutu@gmail.com>
2022-04-08 00:10:42 +0300

    install django registration and create url paths, create login and registration templates

---
+++ awwardsproject/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+from awwardsproject import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('accounts/register/', views.register, name='register'),
+    # path('profile', views.profile, name='profile'),
+    # path('edit-profile', views.editProfile, name='edit-profile'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
