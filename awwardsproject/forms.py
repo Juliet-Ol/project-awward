9e34af88e95499698dcfb6c215b05ebbfe6dc7eb
Juliet <agutu@gmail.com>
2022-04-08 22:59:58 +0300

    create post models add new post form in form.py and register in admin.py

--- awwardsproject/forms.py
+++ awwardsproject/forms.py
@@ -1,7 +1,16 @@
+from dataclasses import fields
 from django import forms
-from .models import Profile
+from .models import Profile, Post
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = '__all__'
+
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['author', 'published_date']
+        fields = '__all__'      
--- awwardsproject/models.py
+++ awwardsproject/models.py
@@ -1,6 +1,7 @@
-import email
+
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
@@ -13,3 +14,24 @@ class Profile(models.Model):
     
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class Post (models.Model):
+    title = models.CharField(max_length=20)
+    post = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    published_date = models.DateTimeField(auto_now_add=True)
+    picture = CloudinaryField('image')
+
+    class Meta:
+        ordering = ['-published_date']  
+
+    @classmethod
+    def display(cls):
+        posts = cls.objects.all()
+        return posts
+
+    def __str__(self):
+        return self.title        
+
+
+        
