4f17edc1712b61263ecedb3a7b4a9710da5dde6e
Juliet <agutu@gmail.com>
2022-04-10 22:57:19 +0300

    include rating path in url

--- awwardsproject/urls.py
+++ awwardsproject/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('profile', views.profile, name='profile'),
     path('edit-profile', views.editProfile, name='edit-profile'),
     path('post', views.post, name='new-post'),
-    path('post', views.PostForm, name='post-form')
+    path('post', views.PostForm, name='post-form'),
+    path('view-post/<int:id>', views.viewPost, name="view-post"),
+    path('rate-post/<rating>/post/<int:post_id>/scale/<int:scale>', views.storeRating, name='store-rating')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
